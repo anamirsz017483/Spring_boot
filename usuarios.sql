30979d94d0e15eb9b6a6695b9c62723d817319f6
anamirsz017483 <anarosa.miranda1a@gmail.com>
2026-03-12 11:22:36 -0300

    Add files via upload

--- usuarios.sql
+++ usuarios.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 24/02/2026 às 13:58
+-- Tempo de geração: 12/03/2026 às 15:22
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -31,22 +31,25 @@ CREATE TABLE `usuarios` (
   `id` bigint(20) NOT NULL,
   `nome` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `senha` varchar(255) NOT NULL,
-  `perfil` varchar(255) DEFAULT NULL,
-  `endereco` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
   `bairro` varchar(255) DEFAULT NULL,
-  `complemento` varchar(255) DEFAULT NULL,
   `cep` varchar(255) DEFAULT NULL,
   `cidade` varchar(255) DEFAULT NULL,
-  `estado` varchar(255) DEFAULT NULL
+  `complemento` varchar(255) DEFAULT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `estado` varchar(255) DEFAULT NULL,
+  `foto` longtext DEFAULT NULL,
+  `perfil` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `perfil`, `endereco`, `bairro`, `complemento`, `cep`, `cidade`, `estado`) VALUES
-(3, 'ana rosa miranda', 'anarosa.miranda1a@gmail.com', 'abc', 'ADMIN', 'Vaifro de Biaggi', 'Campolim', 'casa', '18048-150	Rua Vaifro de Biaggi', 'votorantim', 'sp');
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `bairro`, `cep`, `cidade`, `complemento`, `endereco`, `estado`, `foto`, `perfil`) VALUES
+(1, 'Ana', 'ana@gmail.com ', '1234', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 'ana rosa miranda', 'anarosa.miranda1a@gmail.com', '$2a$10$KDf0/j5QUDsI6ZbVPTPw.uoTtjZe7ZsC1p7qU0rfBwYfcU1NStrE6', 'Campolim', '18048-150	', 'votorantim', 'apartamento', 'Vaifro de Biaggi', 'sp', NULL, 'ADMIN'),
+(5, 'gabriel', 'gabriel.gerim1a123@gmail.com', '$2a$10$srpnYbF98GsIe26rPMLVyu/7oOMjfKBBllpX4d7yDA3ghTrDUo0/W', 'Campolim', '18048-150	', 'votorantim', 'apartamento', 'Vaifro de Biaggi', 'sp', NULL, 'ADMIN');
 
 --
 -- Índices para tabelas despejadas
@@ -67,7 +70,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
